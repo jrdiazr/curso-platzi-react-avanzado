e551fb3d75c41c3063548ee0a16c5fda9b25c991
Jose Roberto Diaz Rodriguez <diazrodriguez.jr@gmail.com>
2020-04-27 22:40:19 -0500

    Avance clase 17

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Category} from '../Category';
 import {List, Item} from './styles';
-import {categories} from '../../../api/db.json';
+
+function useCategoriesData() {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(function () {
+    setLoading(true);
+    fetch('https://petgram-server-jrdiazr-k177zqhah.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response);
+        setLoading(false);
+      })
+  },[])
+
+  return {categories, loading};
+}
 
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const {categories, loading} = useCategoriesData();
+
+  const [showFixed, setFixed] = useState(false);
+
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll);
+
+    return () => document.removeEventListener('scroll', onScroll);
+  },[showFixed])
+
+  const renderList = (fixed)=> (
+    <List fixed={fixed}>
       {
-        categories.map(category => <Item key={category.id}><Category {...category}/></Item>)
+       loading 
+       ? <Item key='loading'><Category/></Item>
+       : categories.map(category => <Item key={category.id}><Category {...category}/></Item>)
       }  
-    </List>    
+    </List>
+  )
+
+  return (
+    <>
+      {
+        renderList()
+      }
+      {
+        showFixed && renderList(true)
+      }
+
+    </>
   );
 }
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 const IMG = "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png"
-import {Img, Button, ImgWrapper} from './styles';
+import {Article, Img, Button, ImgWrapper} from './styles';
 import {MdFavoriteBorder} from 'react-icons/md';
 
 export const PhotoCard = ({id, likes = 0, src = IMG}) => {
+  const ref = useRef(null);
+  const [show, setShow] = useState(false)
+  useEffect(function () {
+    console.log(ref.current);
+    const observer = new window.IntersectionObserver(
+      function (entries) {
+        const {isIntersecting} = entries[0];
+        if (isIntersecting) {
+          setShow(true);
+          observer.disconnect();
+        }
+        
+      }
+    )
+    observer.observe(ref.current);
+  },[ref])
+
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src}/>
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size='32px'/>{likes} likes!
-      </Button>
-    </article>
+    <Article ref={ref}> 
+    {
+      show && 
+      <>
+        <a href={`/detail/${id}`}>
+          <ImgWrapper>
+            <Img src={src}/>
+          </ImgWrapper>
+        </a>
+        <Button>
+          <MdFavoriteBorder size='32px'/>{likes} likes!
+        </Button>
+      </>
+    }
+    </Article>
   )
 }
